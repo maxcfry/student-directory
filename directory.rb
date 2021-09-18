156f9d684cf3cfe5457f85de42bcb88f8e9e2f03
Max Fry <maxcfry@outlook.com>
2021-09-18 14:02:16 +0100

    Print students by iterating over an array

--- directory.rb
+++ directory.rb
@@ -1,17 +1,21 @@
-student_count = 11
+students = [ 
+    "Dr. Hannibal Lecter", 
+    "Darth Vader", 
+    "Nurse Ratched", 
+    "Michael Corleone",
+    "Alex DeLarge",
+    "The Wicked Witch of the West",
+    "Terminator",
+    "Freddy Kruger",
+    "The Joker",
+    "Joeffrey Baratheon",
+    "Norman Bates",
+]
 
 puts "The students of Villains Academy"
 puts "----------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Kruger"
-puts "The Joker"
-puts "Joeffrey Baratheon"
-puts "Norman Bates"
+students.each do |student|
+    puts student
+end
 
-puts "Overall, we have #{student_count} great students"
+puts "Overall, we have #{students.count} great students"
